ac44fcca8e55f9a0e0f247ca2b3992e28c5aabaf
saadalshlowiy <saadalshl04@gmail.com>
2026-04-10 04:41:53 -0700

    Draft 1 concrete classes under STATE

---
+++ AdminDisplayer.jsx
@@ -0,0 +1,23 @@
+import State from "./State" ;
+
+
+export default class AdminDisplayer extends State {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    profileHandler() {
+        //
+    }
+
+    // other actions for this state
+
+    
+
+    render() {
+        //
+    }
+
+}
