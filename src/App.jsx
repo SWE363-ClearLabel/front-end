2a6b9021807120ec3abf121d31708a6f0d4d0940
Hashim-Alramadhan <s202021320@kfupm.edu.sa>
2026-05-02 19:36:24 +0300

    removed references to old components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import UserHeader from './components/UserHeader'
-import UserHome from './components/UserHome'
-import { UserResult } from './components/UserResult'
 import MainPanelCorporate from './components/MainPanelCorporate' 
 import CurrentPanel from './CurrentPanel' 
 
